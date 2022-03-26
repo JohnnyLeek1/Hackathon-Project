656d762ee0e7daaa8cd74980508f07dc0f9e1a44
johnnyleek <leek1j@cmich.edu>
2022-03-26 02:36:55 -0400

    journal page

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import ChoicePage from './pages/ChoicePage';
 import HomePage from './pages/HomePage';
+import JournalPage from './pages/JournalPage';
 import './styles/App.scss';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/choice" element={<ChoicePage />} />
+          <Route path="/journal" element={<JournalPage />} />
           <Route path="/" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
